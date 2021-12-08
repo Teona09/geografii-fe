1e6ab568a3c7c26e0c3fc9c336838ab8cc7f4ae2
Andreea <sturzeandrea@gmail.com>
2021-12-08 20:28:43 +0200

    something

--- src/app/modules/login/register/register.component.ts
+++ src/app/modules/login/register/register.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      repeatPassword: ['', [Validators.required]],
+    }, { validators: this.passwordMatchValidator });
   }
 
+  register() {
+    
+  }
+
+  passwordMatchValidator(control: AbstractControl): void {
+    if (control.get('password').value === control.get('repeatPassword').value) {
+      if (control.get('repeatPassword').hasError('mismatch'))
+        control.get('repeatPassword').setErrors(null);
+    }
+    else {
+      control.get('repeatPassword').setErrors({ mismatch: true });
+    }
+  }
 }
