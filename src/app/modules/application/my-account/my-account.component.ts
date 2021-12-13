8c199e08bec4d6f90b8d0ea2549310fa7fcac53d
Andreea <sturzeandrea@gmail.com>
2021-12-13 20:41:07 +0200

    modal works

--- src/app/modules/application/my-account/my-account.component.ts
+++ src/app/modules/application/my-account/my-account.component.ts
@@ -1,10 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { TokenStorageService } from 'src/app/core/auth/token-storage.service';
 import { User } from 'src/app/core/models/user.model';
 import { UserService } from 'src/app/core/services/user.service';
+import { switchMap, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-my-account',
@@ -36,10 +43,20 @@ export class MyAccountComponent implements OnInit {
 
   ngOnInit(): void {
     this.initializeForm(this.defaultUser);
-    this.userService.getUser(this.tokenStorage.getUser()).subscribe((data) => {
-      this.user = data;
-      this.initializeForm(data);
-    });
+    this.userService
+      .getUser(this.tokenStorage.getUser())
+      .pipe(take(1))
+      .subscribe((data) => {
+        this.user = data;
+        this.initializeForm(data);
+      });
+    this.resetPassForm = this.fb.group(
+      {
+        password: ['', [Validators.required]],
+        repeatPassword: ['', [Validators.required]],
+      },
+      { validators: this.passwordMatchValidator }
+    );
   }
 
   initializeForm(user: User) {
@@ -53,27 +70,50 @@ export class MyAccountComponent implements OnInit {
 
   save() {
     this.user = { ...this.user, ...this.form.value };
-    this.userService.update(this.user).subscribe((data) => {
-      this.user = data;
-      this.initializeForm(data);
-    });
+    this.userService
+      .update(this.user)
+      .pipe(take(1))
+      .subscribe((data) => {
+        this.user = data;
+        this.initializeForm(data);
+      });
   }
 
   delete() {
-    this.userService.delete(this.user.userId).subscribe((data) => {
-      this.router.navigate(['/login']);
-    });
+    this.userService
+      .delete(this.user.userId)
+      .pipe(take(1))
+      .subscribe((data) => {
+        this.router.navigate(['/login']);
+      });
   }
 
-  submit(){
-
+  submit() {
+    this.userService
+      .resetPassword(this.user.userId, this.resetPassForm.get('password').value)
+      .pipe(
+        take(1),
+        switchMap(() => {
+          return this.userService.getUser(this.tokenStorage.getUser());
+        })
+      )
+      .subscribe((data) => {
+        this.user = data;
+        this.initializeForm(data);
+        this.resetPassForm.reset();
+      });
   }
 
   open(content: any) {
-
     this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' });
-
   }
 
-
+  passwordMatchValidator(control: AbstractControl): void {
+    if (control.get('password').value === control.get('repeatPassword').value) {
+      if (control.get('repeatPassword').hasError('mismatch'))
+        control.get('repeatPassword').setErrors(null);
+    } else {
+      control.get('repeatPassword').setErrors({ mismatch: true });
+    }
+  }
 }
