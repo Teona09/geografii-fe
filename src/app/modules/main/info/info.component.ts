53929e673fdeff116eb8983d1a7db363415c2fab
Teona Tanasa <teonatanasa9100@gmail.com>
2021-12-28 20:49:53 +0200

    updated modal fields and structure.

--- src/app/modules/main/info/info.component.ts
+++ src/app/modules/main/info/info.component.ts
@@ -9,17 +9,17 @@ import Data from '../../../../assets/info.json';
 export class InfoComponent implements OnInit {
     public closeResult='';
     public jsonItems = Data.Items;
-    public  infoList: { id: number; title: string; content: string; showbody: boolean; }[] = [];
+    public  infoList: { id: number; content: string; imgSrc:string; showbody: boolean; }[] = [];
     constructor(private modalService: NgbModal) {
     for(var _i=0;_i<this.jsonItems.length;_i++){
-      this.infoList.push({id:(_i+1),title:this.jsonItems[_i].title,content:this.jsonItems[_i].content,showbody:false})
+      this.infoList.push({id:this.jsonItems[_i].id,content:this.jsonItems[_i].content,imgSrc:this.jsonItems[_i].imgSrc,showbody:false})
     }
   }
 
     ngOnInit(): void {}
 
     open(content: any) {
-        this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
+        this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title',size: 'xl', centered: true});
       }
 
       private getDismissReason(reason: any): string {
